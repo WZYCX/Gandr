bdc23d4e201d43d082fca944db8ee061609edfbb
Jude <jude.cowden@protonmail.com>
2022-10-02 20:12:33 +0100

    full functionality

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+import AiClassifyText
+import WordClassDB
+import NTRS
 
 
 app = Flask(__name__, template_folder='static')
+db = WordClassDB.WordClassDB("WordClass copy.db") # WordClassDB.WordClassDB("WordClassDB.db")
+db.clear_duplicates()
 
 
 @app.route('/', methods=['GET'])
@@ -10,7 +16,24 @@ def main():
 
 @app.route('/search', methods=['GET'])
 def search():
-    return render_template('search.html')
+    query = request.args.get('q')
+    keywords = AiClassifyText.classify(query)
+    print(keywords)
+    document_ids = db.get_papers_by_keywords(keywords)[:5]
+
+    results = list()
+    for document_id in document_ids:
+        results.append(NTRS.get_document(document_id).json())
+    print(results[0])
+
+    return render_template(
+        'search.html',
+        query=query,
+        results=results,
+        results_count=len(results),
+        results_plural="s" if len(results) != 1 else "",
+        NTRS=NTRS.HOST
+    )
 
 
 if __name__ == '__main__':
