44fb114d7d74ed837c86a804ea43754941723876
Jude <jude.cowden@protonmail.com>
2022-10-02 20:12:15 +0100

    minor improvements + debugging

--- indexer.py
+++ indexer.py
@@ -8,23 +8,27 @@ import WordClassDB
 
 asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 db = WordClassDB.WordClassDB("WordClass.db")
+db.clear_duplicates()
 
 
 async def async_index_all():
     params = {
         "sort.field": "published",
-        "sort.order": "asc",
+        "sort.order": "desc",
         "page.size": 100,
-        "page.from": 0
+        "page.from": 0,
+        # "published.lte": "2021-03-22"
     }
 
     url = "/api/citations/search?" + urlencode(params)
     json = await NTRS.async_get_json(url)
     total = json["stats"]["total"]
+    total_indexed = 0
     indexed = 0
 
     while True:
         print(params["page.from"], "to", params["page.from"]+99, "out of", total)
+
         for document in json["results"]:
             document_id = document["id"]
 
@@ -47,75 +51,27 @@ async def async_index_all():
             text = await NTRS.async_get_text(url)
 
             # Classifies the text and stores the keywords
-            keywords = AiClassifyText.classify(text, 20)
-            db.add_keywords(document_id, keywords)
-            indexed += 1
-
-        print("Indexed", indexed, "documents", end="\n\n")
-        indexed = 0
-
-        params["page.from"] += params["page.size"]
-        if params["page.from"] >= total:
-            break
-
-        url = "/api/citations/search?" + urlencode(params)
-        json = await NTRS.async_get_json(url)
-
-
-def index_all():
-    params = {
-        "sort.field": "published",
-        "sort.order": "asc",
-        "page.size": 100,
-        "page.from": 0
-    }
-
-    response = NTRS.get("/api/citations/search?" + urlencode(params))
-    data = response.json()
-    total = data["stats"]["total"]
-    indexed = 0
-    total_indexed = 0
-
-    while True:
-        print(params["page.from"], "to", params["page.from"]+99, "out of", total)
-        for document in data["results"]:
-            document_id = document["id"]
-
-            # Checks to see if the document has a file available to be downloaded
-            if not document["downloadsAvailable"]:
-                continue
-
-            url = None
-            for download in document["downloads"]:
-                if "fulltext" in download["links"]:
-                    url = download["links"]["fulltext"]
-
-            if url is None:
-                continue
-
-            if db.already_indexed(document_id):
-                continue
-
-            # Downloads the file associated with the document id
-            response = NTRS.get(url)
-
-            # Classifies the text and stores the keywords
-            keywords = AiClassifyText.classify(response.text, 20)
+            keywords = AiClassifyText.classify(text)[:20]
             db.add_keywords(document_id, keywords)
             indexed += 1
 
         total_indexed += indexed
-        print("Indexed", indexed, "documents", end="\n\n")
-        print("Total Indexed:", total_indexed, end="\n\n")
+        print(f"Indexed {indexed} documents ({total_indexed} so far)", end="\n\n")
         indexed = 0
 
         params["page.from"] += params["page.size"]
         if params["page.from"] >= total:
             break
 
-        response = NTRS.get("/api/citations/search?" + urlencode(params))
-        data = response.json()
+        # NTRS only allows pages up to 10000
+        if params["page.from"] >= 10000:
+            print("Searching documents from before", document["distributionDate"][:10])
+            params["published.lte"] = document["distributionDate"][:10]
+            params["page.from"] = 0
+
+        url = "/api/citations/search?" + urlencode(params)
+        json = await NTRS.async_get_json(url)
+        total = json["stats"]["total"]
 
 
-# index_all()
 asyncio.run(async_index_all())
