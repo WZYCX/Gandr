5fe89e8976701ada9dbf6bc4676eda7c4d9281bf
Jude <jude.cowden@protonmail.com>
2022-10-02 18:59:21 +0100

    database functions
    + get_papers_by_keywords: takes a list of keywords and then returns a list of sorted document IDs
    + clear_duplicates: removes any duplicate rows (legacy)

--- WordClassDB.py
+++ WordClassDB.py
@@ -17,10 +17,24 @@ class WordClassDB:
     def get_papers_by_paper(self, paper):
         self.c.execute('SELECT * FROM words WHERE paper=?', (paper,))
         return self.c.fetchone()
-    
+
     def get_papers_by_keyword(self, keyword):
         self.c.execute('SELECT paper FROM words WHERE keyword=?', (keyword,))
         return self.c.fetchall()
+
+    def get_papers_by_keywords(self, keywords):
+        def key(paper):
+            return count_document_ids[paper]
+        document_ids = list()
+        for keyword in keywords:
+            document_ids += self.get_papers_by_keyword(keyword)
+        count_document_ids = dict()
+        for document_id in document_ids:
+            if document_id[0] in count_document_ids:
+                count_document_ids[document_id[0]] += 1
+            else:
+                count_document_ids[document_id[0]] = 1
+        return sorted(count_document_ids.keys(), key=key, reverse=True)
 
     def get_all(self):
         self.c.execute('SELECT * FROM words')
@@ -36,7 +50,7 @@ class WordClassDB:
 
     def __del__(self):
         self.conn.close()
-    
+
     def add_keywords(self,paper,keywords):
         for keyword in keywords:
             self.c.execute('INSERT INTO words VALUES (?, ?)', (paper, keyword))
@@ -45,6 +59,10 @@ class WordClassDB:
     def already_indexed(self, paper):
         self.c.execute('SELECT * FROM words WHERE paper=?', (paper,))
         return self.c.fetchone() is not None
+
+    def clear_duplicates(self):
+        self.c.execute('DELETE FROM words WHERE rowid NOT IN (SELECT MIN(rowid) FROM words GROUP BY paper, keyword)')
+        self.conn.commit()
 
 
 #WordClassDB("test.db").add_paper(1,"test")
